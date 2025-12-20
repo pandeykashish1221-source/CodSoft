739746d574fecc8c21f56d244d276968de2b0351
kashish Pandey <pandeykashish1221@gmail.com>
2025-12-20 21:40:16 +0530

    Initial commit - Quiz Application by Kashish Pandey

---
+++ QuizApplication-master/app/src/main/java/com/example/android/quizapplication/HowTo.java
@@ -0,0 +1,18 @@
+/**
+ * Quiz Application
+ * Created by: Kashish Pandey
+ * How To Activity - Instructions for using the app
+ */
+package com.example.android.quizapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HowTo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_to);
+    }
+}
